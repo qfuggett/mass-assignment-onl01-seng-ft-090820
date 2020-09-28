b22f5b382dc116efaf5cfe17b4065065a11715ba
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-28 03:08:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,10 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height
 
-  def initialize(:name, :birthday, :hair_color, :eye_color, :height)
-  
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
   end
 
 
